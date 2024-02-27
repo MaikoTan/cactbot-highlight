aa99e77ea4d68dd53f6441ba983d57fa25bb1e39
Maiko Tan <maiko.tan.coding@gmail.com>
2024-02-27 16:05:15 +0800

    refactor: push disposable in once

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { commands, languages, workspace, ExtensionContext } from 'vscode'
 
 import {
@@ -11,33 +9,25 @@ import {
   TimelineCodeLensProvider,
 } from './timeline'
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext): void {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  context.subscriptions.push(commands.registerCommand('cactbot.timeline.incDecTime', () => incDecTime()))
-  context.subscriptions.push(commands.registerCommand('cactbot.timeline.setTime', () => setTime()))
-
-  // register translate timeline
   context.subscriptions.push(
+    commands.registerCommand('cactbot.timeline.incDecTime', () => incDecTime()),
+    commands.registerCommand('cactbot.timeline.setTime', () => setTime()),
+
+    // register translate timeline
     workspace.registerTextDocumentContentProvider('cactbot-timeline', translatedTimelineProvider),
-  )
-  context.subscriptions.push(commands.registerCommand('cactbot.timeline.translate', () => translateTimeline()))
 
-  context.subscriptions.push(languages.registerCodeLensProvider('cactbot-timeline', new TimelineCodeLensProvider()))
-  context.subscriptions.push(
+    commands.registerCommand('cactbot.timeline.translate', () => translateTimeline()),
+
+    languages.registerCodeLensProvider('cactbot-timeline', new TimelineCodeLensProvider()),
     commands.registerCommand('cactbot.timeline.runGenerateScript', (...args) => {
       runMakeTimeline(args)
     }),
-  )
-  context.subscriptions.push(
+
     commands.registerCommand('cactbot.timeline.runGenerateScriptWithoutExecution', (...args) => {
       runMakeTimeline(args, false)
     }),
   )
 }
 
-// this method is called when your extension is deactivated
 // export function deactivate() { }
